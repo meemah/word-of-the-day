164ce0b823d61a1a5b6690368d7210818167b4ed
meemah <maimunahibro94@gmail.com>
2025-02-16 16:14:36 +0100

    chore: adjust repo to return model

--- lib/repository/word_repository.dart
+++ lib/repository/word_repository.dart
@@ -1,7 +1,8 @@
+import 'package:wotd/model/words_of_day_response.dart';
 import 'package:wotd/service/api_service.dart';
 
 abstract class IWordRepository {
-  Future getWordOfTheDay();
+  Future<WordsOfDayListResponse> getWordOfTheDay();
 }
 
 class WordRepository extends IWordRepository {
@@ -9,7 +10,8 @@ class WordRepository extends IWordRepository {
 
   WordRepository({required this.apiService});
   @override
-  Future getWordOfTheDay() async {
-    await apiService.get("/random");
+  Future<WordsOfDayListResponse> getWordOfTheDay() async {
+    var response = await apiService.get("/random");
+    return WordsOfDayListResponse.fromJson(response);
   }
 }
